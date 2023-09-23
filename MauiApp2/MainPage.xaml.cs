13fb3974e6df1d8b5a29b67d3fd5ae2e9c64eb2f
Hannah <Hannah@ICHI>
2023-09-23 16:58:09 +0800

    Add project files.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void imageButton_Clicked(object sender, EventArgs e)
+        {
+            imageButton.Source = "second.jpg";
+            imgBtn.Text = "Reality";
+        }
+
+        private void imageButton2_Clicked(object sender, EventArgs e)
+        {
+            imageButton2.Source = "two.jpeg";
+            imgBtn2.Text = "Reality";
+        }
+    }
+}
